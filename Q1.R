e7dc5851702c1c907e927d2e637c413982b8f2f5
Nukesh2056 <99228157+Nukesh2056@users.noreply.github.com>
2025-03-22 12:17:41 +0530

    Create Q1.R

---
+++ Q1.R
@@ -0,0 +1,6 @@
+# Create a logical vector of length 12 that alternates between TRUE and FALSE.
+
+alternating_vector <- rep(c(TRUE, FALSE), 6)
+
+# Print the resulting vector.
+print(alternating_vector)
